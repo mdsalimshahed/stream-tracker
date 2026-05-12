28f2f17774ddebf7e99df845616bb6869740c5b4
Md Salim Shahed Shajid <mdsalimshahed@gmail.com>
2026-05-12 23:42:59 +0600

    Bandaid #2

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import { cloudflare } from "@cloudflare/vite-plugin";
 
 export default defineConfig({
-  plugins: [tailwindcss(), react(), cloudflare()],
+  plugins: [tailwindcss(), react()],
   server: {
     proxy: {
       '/steam-api': {
